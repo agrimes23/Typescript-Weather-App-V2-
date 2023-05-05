433c74b5b809d30d49a8533ab0e5d818372f7c8a
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-05-04 22:39:31 -0500

    added html elements to the homepage. Setting up open weather api call

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const Homepage = (props: any) => {
 
-    const submitCity = () => {
+    const [citySearch, setCitySearch] = useState("")
+    const navigate = useNavigate()
+
+    const handleChange = (e: any) => {
+        
+    }
+
+    const handleSubmit = () => {
         props.getCityInfo();
     }
 
     return (
         <>
-            <p>Hellooo</p>
+            <form onSubmit={handleSubmit}>
+                <div className="hp-container">
+                    <div>
+                        <h1>Enter a city name to view the weather: </h1>
+                    </div>
+                    <div>
+                        <div>
+                            <label htmlFor="enter a city name"></label>
+                            <input type="text" className="" placeholder='Search City' name="city" onChange={handleChange}/>
+                        </div>
+                        <div>
+                            <input type="submit" className="" value="Get Weather Info"/>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </>
     )
 }
